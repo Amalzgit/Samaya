73b8b6240df15c1ffbf056ea53d2dd1b1706fb9f
Amalzgit <1mailtoamal@gmail.com>
2024-08-01 21:35:51 +0530

    addres crud

--- routes/userRoutes/userdetialsRoute.js
+++ routes/userRoutes/userdetialsRoute.js
@@ -17,7 +17,13 @@ const isUser = (req, res, next) => {
 
 // userdetials_route.use(noCache)
 // userdetials_route.use(isUser)
-userdetials_route.get('/user-details', isUser,userDetails.loadUserDetails)
-userdetials_route.post('/user-details', isUser, userDetails.updateUSer)
+userdetials_route.get('/user-details', isUser,userDetails.loadUserDetails);
+userdetials_route.post('/user-details', isUser, userDetails.updateUser);
+userdetials_route.get('/add-address', isUser, userDetails.loadAddAddress);
+userdetials_route.post('/add-address', userDetails.addAddress);
+userdetials_route.get('/edit-address/:addressId',userDetails.loadEditAddress)
+userdetials_route.post('/edit-address/:addressId',userDetails.editAddress)
+userdetials_route.get('/delete-address/:addressId',userDetails.deleteAddress)
+
 
 module.exports =userdetials_route  
